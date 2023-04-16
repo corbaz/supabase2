b6cd3aec8a38cb985cc140bbd0d877b9bb440dbe
Julio Cesar Corbaz <juliocesar.corbaz@emeal.nttdata.com>
2023-04-16 09:08:15 -0300

    Commit realizado el 2023-04-16 09:08:14

--- github-gitlab.py
+++ github-gitlab.py
@@ -1,16 +1,48 @@
+import argparse
 import os
-import sys
-import datetime
+from datetime import datetime
 
-# Obtener el mensaje del commit y la rama (si se proporcionan)
-commit_msg = sys.argv[1] if len(sys.argv) > 1 else datetime.datetime.now().strftime("Commit del %Y-%m-%d %H:%M:%S")
-branch = sys.argv[2] if len(sys.argv) > 2 else 'main'
+# Obtener la fecha y hora actual
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# Ejecutar los comandos de git
-os.system('git add .')
-os.system(f'git commit -m "{commit_msg}"')
-os.system(f'git push github {branch}')
-os.system(f'git push gitlab {branch}')
+# Mensaje por defecto si no se proporciona uno
+default_commit_message = f"Commit realizado el {now}"
+
+
+# Función que ejecuta los comandos de git
+def git_push(commit_message=default_commit_message, branch='main'):
+    os.system('git add .')
+    os.system(f'git commit -m "{commit_message}"')
+    os.system(f'git push github {branch}')
+    os.system(f'git push gitlab {branch}')
+    os.system(f'git push heroku {branch}')
+    os.system('heroku open')
+
+
+# Parsear los argumentos
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--message', default=default_commit_message,
+                    help='Mensaje para el commit')
+parser.add_argument('-b', '--branch', default='main',
+                    help='Rama para hacer el push')
+args = parser.parse_args()
+
+# Ejecutar la función con los argumentos parseados
+git_push(args.message, args.branch)
+
+# import os
+# import sys
+# import datetime
+#
+# # Obtener el mensaje del commit y la rama (si se proporcionan)
+# commit_msg = sys.argv[1] if len(sys.argv) > 1 else datetime.datetime.now().strftime("Commit del %Y-%m-%d %H:%M:%S")
+# branch = sys.argv[2] if len(sys.argv) > 2 else 'main'
+#
+# # Ejecutar los comandos de git
+# os.system('git add .')
+# os.system(f'git commit -m "{commit_msg}"')
+# os.system(f'git push github {branch}')
+# os.system(f'git push gitlab {branch}')
 
 # import argparse
 # import os
@@ -40,4 +72,3 @@ os.system(f'git push gitlab {branch}')
 # for cmd in ['git add .', 'git commit -m "Primer commit"', 'git push github main', 'git push gitlab main']:
 #     output = os.popen(cmd).read()
 #     print(output)
-
